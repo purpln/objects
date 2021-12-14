f50c2e0435d9e322fc0734bc6247eeb7f99083e7
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-12-14 14:35:34 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Objects",
     platforms: [.iOS(.v10)],
-    products: [.library(name: "Objects", targets: ["Objects"])],
+    products: [.library(name: "Objects", targets: ["Objects", "Coding", "Observable", "Dependancy"])],
     dependencies: [],
-    targets: [.target(name: "Objects")]
+    targets: [.target(name: "Objects"), .target(name: "Coding"), .target(name: "Observable"), .target(name: "Dependancy")]
 )
