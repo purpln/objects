5e443d8700e309616fe4cdaa376d937bf2b379b2
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-02-07 17:39:43 +0300

    no message

--- Sources/Dependancy/Dependancy.swift
+++ Sources/Dependancy/Dependancy.swift
@@ -1,1 +1,35 @@
+import Foundation
 
+public protocol AnyDependent { }
+private extension AnyDependent {
+    init(_ closure: () -> Self) { self = closure() }
+}
+public protocol Dependent: AnyObject, AnyDependent {
+    associatedtype Dependency: DependencyProtocol
+    var dependency: Dependency { get set }
+}
+public extension Dependent {
+    init(_ dependency: Dependency, _ init: @autoclosure () -> Self){
+        Dependency.dependency = dependency
+        self.init(`init`)
+        Dependency.dependency = nil
+    }
+}
+public protocol DependencyProtocol { init() }
+public extension DependencyProtocol {
+    static func setup(_ init: @autoclosure ()->(Self) = .init()) -> Self{
+        if let dependency = dependency {
+            self.dependency = nil
+            return dependency
+        } else {
+            return `init`()
+        }
+    }
+}
+private extension DependencyProtocol {
+    private static var dependencyKey: String { "DependencyKey" }
+    static var dependency: Self? {
+        set(value){ Thread.current.threadDictionary[dependencyKey] = value }
+        get{ Thread.current.threadDictionary[dependencyKey] as? Self }
+    }
+}
